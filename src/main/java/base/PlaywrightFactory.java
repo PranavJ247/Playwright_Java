8d725254319425dc10f22e6ffc0dc7ffaf0510b6
pranavj247 <pranavjadhav831@gmail.com>
2026-08-03 00:11:12 +0530

    3 aug commit

--- src/main/java/base/PlaywrightFactory.java
+++ src/main/java/base/PlaywrightFactory.java
@@ -1,4 +1,54 @@
 package base;
 
+import com.microsoft.playwright.*;
+
 public class PlaywrightFactory {
+
+    private static ThreadLocal<Playwright> playwright = new ThreadLocal<>();
+    private static ThreadLocal<Browser> browser = new ThreadLocal<>();
+    private static ThreadLocal<BrowserContext> context = new ThreadLocal<>();
+    private static ThreadLocal<Page> page = new ThreadLocal<>();
+
+    public static void initBrowser() {
+        playwright.set(Playwright.create());
+        BrowserType.LaunchOptions options = new BrowserType.LaunchOptions();
+        options.setHeadless(false);
+
+        // Launch Browser
+        browser.set(
+                playwright.get().chromium().launch(options)
+        );
+        context.set(
+                browser.get().newContext()
+        );
+        page.set(
+                context.get().newPage()
+        );
+    }
+    public static Page getPage() {
+        return page.get();
+    }
+
+    public static void closeBrowser() {
+
+        if (page.get() != null) {
+            page.get().close();
+            page.remove();
+        }
+
+        if (context.get() != null) {
+            context.get().close();
+            context.remove();
+        }
+
+        if (browser.get() != null) {
+            browser.get().close();
+            browser.remove();
+        }
+
+        if (playwright.get() != null) {
+            playwright.get().close();
+            playwright.remove();
+        }
+    }
 }
